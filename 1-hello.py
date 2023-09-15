56c48c477f669168c478d0e8982bf12dd9061988
Shubham <shm.code1@gmail.com>
2023-09-16 00:40:19 +0530

    helloPython-dataTypes

---
+++ 1-hello.py
@@ -0,0 +1,6 @@
+print("hello")
+name = input("Enter name ")  #initialising variable
+print("Hello" ,name)  #taking input
+favoriteSub=input("Enter your favorite subject ")
+print("Your favorite Subject is",favoriteSub)
+#------------------------------------------------------------------------------#
---
+++ 2-dataType.py
@@ -0,0 +1,15 @@
+#Data Type in Python Mutable and immutable
+# int,float,etc are not immutable
+a=10  # python decide its type automatiacally
+print(type(a))
+b=5.2
+print(type(b))
+c=True
+print(type(c))
+d="g"
+print(type(d))
+e="sam"
+print(type(e))
+num1=int(input("Enter first num1 "))   # converting it to integer type
+num2=int(input("Enter second num2 "))
+print(num1+num2)
